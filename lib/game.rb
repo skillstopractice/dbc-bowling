e3bb9d372e16ba3caefd182f2940474f52093c03
Gregory Brown <gregory@practicingdeveloper.com>
2025-01-10 12:53:03 -0500

    First failing contract

--- lib/game.rb
+++ lib/game.rb
@@ -2,4 +2,7 @@ require_relative "../protocol/game"
 
 class Game
   prepend Protocol::Game
+
+  def score
+  end
 end
--- protocol/game.rb
+++ protocol/game.rb
@@ -2,6 +2,10 @@ require_relative "../vendor/contractor"
 
 module Protocol
   module Game
-
+    def initialize
+      Contractor.for("A newly created game")
+                .ensures("score begins at zero") { |result| score == 0 }
+                .work { super }
+    end
   end
 end
