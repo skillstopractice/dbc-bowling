35cb67d29657fb4a6a723e194e3dbc7fde026a17
Gregory Brown <gregory@practicingdeveloper.com>
2025-01-10 14:11:35 -0500

    Temporarily walk back strike + spare detection

--- protocol/game.rb
+++ protocol/game.rb
@@ -28,16 +28,16 @@ module Protocol
       end
 
 
-      if ball_scores.values_at(-2,-1).compact.sum < 10
-        non_bonus_condition = "game score will increase by exactly the ball score amount if " +
-                              "the last two ball scores before it add up to less than 10"
+      # if ball_scores.values_at(-2,-1).compact.sum < 10
+      #   non_bonus_condition = "game score will increase by exactly the ball score amount if " +
+      #                         "the last two ball scores before it add up to less than 10"
 
-        contractor.ensures(non_bonus_condition) do |result, diff|
-          diff[:score][:after] == diff[:score][:before] + ball_score
-        end
-      else
-        contractor.broken("Game#roll does not yet handle spares or strikes")
-      end
+      #   contractor.ensures(non_bonus_condition) do |result, diff|
+      #     diff[:score][:after] == diff[:score][:before] + ball_score
+      #   end
+      # else
+      #   contractor.broken("Game#roll does not yet handle spares or strikes")
+      # end
 
       contractor.work { super }
     end
